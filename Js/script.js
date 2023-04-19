20d2218535c0f14ebbc8dcc6a8befa5c2e762b60
iiiiiuri <iurilima.395@gmail.com>
2023-04-19 15:14:37 -0300

    Commit Organização
    -Organizar o código

--- Js/script.js
+++ Js/script.js
@@ -1,106 +1,98 @@
 
 
 //CRIANDO CONSTANTES E VARIÁVEIS PARA VERIFICAÇÃO DO CÓDIGO
-const forms      =         document.getElementById('form');
-const inputEmail =         document.getElementById('email');
-const inputUser  =         document.getElementById('user');
-const inputSenha =         document.getElementById('senha');
-const erroEmail  =         document.getElementById('erroEmail');
-const erroPass   =         document.getElementById('erroPass');
-const erroUser   =         document.getElementById('erroUser');
-const signup      =         document.getElementById('signup');
-const emailRegex =         /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-const senhaOuEmailIncorretos  =         document.getElementById('incorretPass')
+const forms = document.getElementById('form');
+const inputEmail = document.getElementById('email');
+const inputUser = document.getElementById('user');
+const inputSenha = document.getElementById('senha');
+const erroEmail = document.getElementById('erroEmail');
+const erroPass = document.getElementById('erroPass');
+const erroUser = document.getElementById('erroUser');
+const signup = document.getElementById('signup');
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const senhaOuEmailIncorretos = document.getElementById('incorretPass')
 
 
 
 //Função que altera as propriedades do botão "SignUp". - - - - -  Desativando e ativando o mesmo.
 //Função que altera propriedades do campo inputEmail.  - - - - -  Exibe o campo Span erroEmail, caso ele não seja válido.
 
-function emailValidate(){
-    if(emailRegex.test(inputEmail.value)){
-        inputEmail.style.cssText = 
-       'border: 2px solid green;';
-       erroEmail.style.cssText =
-        'display: none;';
+function emailValidate() {
+    if (emailRegex.test(inputEmail.value)) {
+        inputEmail.style.cssText =
+            'border: 2px solid green;';
+        erroEmail.style.cssText =
+            'display: none;';
         document.getElementById('signup').disabled = false;
         signup.style.cssText =
-       'background: #01ACEA;';
- 
+            'background: #01ACEA;';
 
-    
+
+
     }
-    else{
+    else {
         erroEmail.style.cssText =
-        'display: inline;'+
-        'color: #FA901E;'+
-       'font-weight: bold;';
-        inputEmail.style.cssText = 
-       'border: 2px solid #AD600E;';
-       signup.style.cssText =
-       'background: #8a8a8a;'+
-       'box-shadow: none;';
-       document.getElementById('signup').disabled = true;
+            'display: inline;' +
+            'color: #FA901E;' +
+            'font-weight: bold;';
+        inputEmail.style.cssText =
+            'border: 2px solid #AD600E;';
+        signup.style.cssText =
+            'background: #8a8a8a;' +
+            'box-shadow: none;';
+        document.getElementById('signup').disabled = true;
     }
- 
+
 }
 
 
 //Função que altera as propriedades do campo inputSenha. - - - - -  Exibe o campo Span erroPass, caso ele não seja maior ou igual a 8 digitos.
-function senhaValidate(){
-    if(inputSenha.value.length < 8 ){
+function senhaValidate() {
+    if (inputSenha.value.length < 8) {
 
         erroPass.style.cssText =
-        'display: inline;'+
-        'color: #FA901E;'+
-       'font-weight: bold;';
-        inputSenha.style.cssText = 
-       'border: 2px solid #AD600E;';
-      
-        
+            'display: inline;' +
+            'color: #FA901E;' +
+            'font-weight: bold;';
+        inputSenha.style.cssText =
+            'border: 2px solid #AD600E;';
 
-    }else{
-        inputSenha.style.cssText = 
-       'border: 2px solid green;';
-       erroPass.style.cssText =
-        'display: none;';
-     
+
+
+    } else {
+        inputSenha.style.cssText =
+            'border: 2px solid green;';
+        erroPass.style.cssText =
+            'display: none;';
+
     }
-    }
+}
 
 
 //Função que altera as propriedades do campo inputUser. - - - - - Exibe o campo Span:erroUser, caso ele não seja maior ou igual a 4 digitos.
-    function userValidate(){
-    if(inputUser.value.length < 4 ){
+function userValidate() {
+    if (inputUser.value.length < 4) {
 
         erroUser.style.cssText =
-        'display: inline;'+
-        'color: #FA901E;'+
-       'font-weight: bold;';
-        inputUser.style.cssText = 
-       'border: 2px solid #AD600E;';
-       
-        
+            'display: inline;' +
+            'color: #FA901E;' +
+            'font-weight: bold;';
+        inputUser.style.cssText =
+            'border: 2px solid #AD600E;';
 
-    }else{
-        inputUser.style.cssText = 
-       'border: 2px solid green;';
-       erroUser.style.cssText =
-        'display: none;';
-        
+
+
+    } else {
+        inputUser.style.cssText =
+            'border: 2px solid green;';
+        erroUser.style.cssText =
+            'display: none;';
+
     }
-    }
+}
 
 
-        
-    
-    $('.Carrosel').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        adaptiveHeight: true
-      });
+
 
 
 
--- about.php
+++ about.php
@@ -1,50 +1,47 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-   <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Home</title>
-
- 
-
-   </head>
-   <link rel="stylesheet" href="stylesheet/stylesAbout.css">
-   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-   <link href="https://fonts.googleapis.com/css2?family=K2D:wght@100;700&display=swap" rel="stylesheet">
-
-   <body>
-      <header class = "cabecalho">
-         <a class = "menuToggle">
-         <img src="docs/Toggle.svg"></img></a>
-         
-         
-         <div class ="logo">
-            <img src="docs/DB-BLUE.svg" alt="logo" width="504" height="400"></img>
-         </div>
-         <nav class = "menu">
-            <ul>
-               <li><a href="home.php">Home</a></li>
-               <li><a href="about.php">About</a></li>
-               <li><a href="#Contact">Contact</a></li>
-               <li><a href="#Price">Price</a></li>
-            </ul>          
-         </nav>
-      </header>
-
-      <div class="main" >
-            <div><h1>Services</h1><img id ="primeira" src="docs/carrosel1.svg" > </img><p>We believe that every customer is unique, which is why we offer a wide range of cloud storage solutions that can be customized to meet each customer's needs.</p></div>    
-            <div><h1>Team</h1><img id ="segunda" src="docs/carrosel2.svg" > </img><p>Our team is made up of experienced and talented professionals, who are always looking for new ideas and trends to keep our company at the forefront of innovation.</p></div>
-            <div><h1>Commitment</h1><img id ="terceira" src="docs/carrosel3.svg" > </img><p>We value transparency in everything we do, working closely with our customers to understand their needs and provide the most appropriate solutions.</p></div>
- 
-
-            </div>
-            <div class = "fim"><img src="docs/DB-BLUE.svg"  width="190" height="100"></div>
-            
-      
-
-				
-
-
-   </body>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+  </head>
+  <link rel="stylesheet" href="stylesheet/stylesAbout.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=K2D:wght@100;700&display=swap" rel="stylesheet">
+  <body>
+    <header class = "cabecalho">
+      <a class = "menuToggle">
+      <img src="docs/Toggle.svg"></img></a>
+      <div class ="logo">
+        <img src="docs/DB-BLUE.svg" alt="logo" width="504" height="400"></img>
+      </div>
+      <nav class = "menu">
+        <ul>
+          <li><a href="home.php">Home</a></li>
+          <li><a href="about.php">About</a></li>
+          <li><a href="#Contact">Contact</a></li>
+          <li><a href="#Price">Price</a></li>
+        </ul>
+      </nav>
+    </header>
+    <div class="main" >
+      <div>
+        <h1>Services</h1>
+        <img id ="primeira" src="docs/carrosel1.svg" > </img>
+        <p>We believe that every customer is unique, which is why we offer a wide range of cloud storage solutions that can be customized to meet each customer's needs.</p>
+      </div>
+      <div>
+        <h1>Team</h1>
+        <img id ="segunda" src="docs/carrosel2.svg" > </img>
+        <p>Our team is made up of experienced and talented professionals, who are always looking for new ideas and trends to keep our company at the forefront of innovation.</p>
+      </div>
+      <div>
+        <h1>Commitment</h1>
+        <img id ="terceira" src="docs/carrosel3.svg" > </img>
+        <p>We value transparency in everything we do, working closely with our customers to understand their needs and provide the most appropriate solutions.</p>
+      </div>
+    </div>
+    <div class = "fim"><img src="docs/DB-BLUE.svg"  width="190" height="100"></div>
+  </body>
 </html>
