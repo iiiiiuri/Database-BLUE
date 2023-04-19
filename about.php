21c086f1850b3d935ce6309dd358eb34686e528a
iiiiiuri <iurilima.395@gmail.com>
2023-04-19 08:08:17 -0300

    Criação da Janela "About"
    -Criar Janela About.
    -Fazer a estilização da mesma.
    -Card Hover landspace.
    -Card Click Portrait.

---
+++ about.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Home</title>
+   </head>
+   <link rel="stylesheet" href="stylesheet/styleAbout.css">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=K2D:wght@100;700&display=swap" rel="stylesheet">
+   <body>
+      <header class = "cabecalho">
+         <a class = "menuToggle">
+         <img src="docs/Toggle.svg"></img></a>
+         
+         
+         <div class ="logo">
+            <img src="docs/DB-BLUE.svg" alt="logo" width="504" hidth="400"></image>
+         </div>
+         <nav class = "menu">
+            <ul>
+               <li><a href="#Home">Home</a></li>
+               <li><a href="#About">About</a></li>
+               <li><a href="#Contact">Contact</a></li>
+               <li><a href="#Price">Price</a></li>
+            </ul>          
+         </nav>
+      </header>
+
+      <div class="main" >
+            <div class = "esquerda/topo">
+
+            </div>
+            <div class = "meio">
+
+            </div>
+            <div class = "direita/inf">
+
+            </div>
+         </div>
+   </body>
+</html>
